75e6b9a3f7924ee69e71a6a189661c2269013511
matt manes <mattmanes@pm.me>
2023-12-24 10:01:34 -0600

    chore: build v1.0.0

--- tests/test_lanutils.py
+++ tests/test_lanutils.py
@@ -1,15 +1,15 @@
 from lanutils import lanutils
 
 
-def test__lanutils__get_myip():
+def test__lanutils__get_my_ip():
     # Not gonna work if you aren't connected to your network
-    assert len(lanutils.get_myip()) > 0
-    assert any(ip[2] in ["Ethernet", "Wi-Fi"] for ip in lanutils.get_myip())
-    assert len(lanutils.get_myip(None)) > 0
+    assert len(lanutils.get_my_ip()) > 0
+    assert any(ip[2] in ["Ethernet", "Wi-Fi"] for ip in lanutils.get_my_ip())
+    assert len(lanutils.get_my_ip(None)) > 0
 
 
 def test__lanutils__ip_is_alive():
-    assert lanutils.ip_is_alive(lanutils.get_myip()[0][0])
+    assert lanutils.ip_is_alive(lanutils.get_my_ip()[0][0])
 
 
 def test__lanutils__enumerate_devices():
@@ -17,19 +17,19 @@ def test__lanutils__enumerate_devices():
 
 
 # Start a http server in shell with command
-# "py -m http.server 8000 -b {whatever is returned by lanutils.get_myip()[0][0]}"
+# "py -m http.server 8000"
 # before running the following tests.
 def test__lanutils__port_is_open():
-    myip = lanutils.get_myip()[0][0]
+    myip = lanutils.get_my_ip()[0][0]
     port = 8000
     assert lanutils.port_is_open(myip, port)
 
 
 def test__lanutils__scan_ports():
-    myip = lanutils.get_myip()[0][0]
+    myip = lanutils.get_my_ip()[0][0]
     assert 8000 in lanutils.scan_ports(myip, (7999, 8001))
 
 
 def test__lanutils__get_available_port():
-    myip = lanutils.get_myip()[0][0]
-    assert lanutils.get_available_port(myip, (8000, 8100)) > 8000
+    myip = lanutils.get_my_ip()[0][0]
+    assert lanutils.get_available_port(myip, (8000, 8100)) >= 8000
